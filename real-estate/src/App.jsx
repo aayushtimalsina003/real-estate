ae1ab6cb4f99b85e7143ad3dbee2e122d7947736
Aayush Timalsina <aayushtimalsina2@gmail.com>
2024-11-24 22:48:39 +0545

    Contact Page Completed

--- real-estate/src/App.jsx
+++ real-estate/src/App.jsx
@@ -3,14 +3,19 @@ import Header from "./components/Header.jsx";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
 import Testimonials from "./components/Testimonials.jsx";
+import Contact from "./components/Contact.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
       <Testimonials />
+      <Contact />
     </div>
   );
 };
